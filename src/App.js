9e5e3fcaa50f4915a9b39183e02cfc0a0942dc88
Randy Kim <rhkim1292@gmail.com>
2022-12-28 00:37:22 -0800

    Implement card shuffling method

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+import { useState } from 'react';
+import Card from './components/Card.js';
+
 function App() {
-	return <div className="App"></div>;
+	// const [bestScore, setBestScore] = useState(0);
+	// const [currScore, setCurrScore] = useState(0);
+	const [cardList, setCardList] = useState([
+		0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14,
+	]);
+
+	const shuffle = (arr) => {
+		const arrCopy = [...arr];
+		let currIdx = arrCopy.length;
+		let randomIdx;
+
+		while (currIdx !== 0) {
+			randomIdx = Math.floor(Math.random() * currIdx);
+			currIdx--;
+
+			[arrCopy[currIdx], arrCopy[randomIdx]] = [
+				arrCopy[randomIdx],
+				arrCopy[currIdx],
+			];
+		}
+
+		return arrCopy;
+	};
+
+	const handleClick = (e) => {
+		setCardList(shuffle(cardList));
+	};
+
+	return (
+		<div className="App">
+			{cardList.map((currCard, idx) => {
+				return <Card key={idx} name={currCard} />;
+			})}
+			<button onClick={handleClick}>Shuffle!</button>
+		</div>
+	);
 }
 
 export default App;
